847cca531b59ad8512ab470c1fe71673b07c7755
Kristina <crsbka@bk.ru>
2022-04-27 09:28:34 -0400

    New components

--- src/Result.js
+++ src/Result.js
@@ -1,27 +1,10 @@
 import React from "react";
 import './App.css';
-import Navbar from "./Navbar";
-import Search from "./Search";
-import Card from "./Card";
-
-import About from "./About";
-// import Market from "./Market";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 export default function Result() {
     return (
-         <Router>
-
             <div className="Result">
-
-
-
-
-
-
+              {/*пока не знаю чо тут будет нужно ли ваще чото */}
             </div>
-
-         </Router>
-
     );
 }
